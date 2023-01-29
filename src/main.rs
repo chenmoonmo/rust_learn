f66dcc239d716c4d104b2021e8ceaa54b6814fa7
chenmoonmo <chenmoonmo@gmail.com>
2023-01-29 17:42:04 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod mods;
+use std::ops;
+
+fn get_last<T: ops::Mul + Copy, const N: usize>(arr: &[T; N]) -> T {
+    let len = arr.len();
+    arr[len-1]
+}
 
 fn main() {
-    mods::about_impl::about_impl();
+    let arr: [i32; 2] = [3; 2];
+    get_last(&arr);
 }
